f2effae4196ee329aec1053f5006136da1b55d70
KennethMinn <minthukha2472005@gmail.com>
2024-03-25 20:35:46 +0630

    Hero is also a Person, same type

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -2,7 +2,8 @@ package inheritance;
 
 public class InheritanceDemo {
     public static void main(String[] args) {
-        Hero hero = new Hero("mtk", 18, "Coder");
+        Person hero = new Hero("mtk", 18, "Coder");
+        Hero hero1 = new Hero("mtk", 18, "Coder");
         System.out.println(hero.toString());
     }
 }
