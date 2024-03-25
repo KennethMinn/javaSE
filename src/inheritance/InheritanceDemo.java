01651fa2f7d43b31389bc27a1f7db3a92e308980
KennethMinn <minthukha2472005@gmail.com>
2024-03-25 20:15:34 +0630

    Declare Inheritance class to demonstrate inheritance

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -1,4 +1,8 @@
 package inheritance;
 
 public class InheritanceDemo {
+    public static void main(String[] args) {
+        Hero hero = new Hero("mtk", 18, "Coder");
+        System.out.println(hero.toString());
+    }
 }
