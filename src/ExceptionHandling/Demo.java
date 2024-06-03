4022e5f76c837bfcef9b61b7883188068f82b9b7
KennethMinn <minthukha2472005@gmail.com>
2024-06-03 21:04:23 +0630

    Abstraction and exception handling

---
+++ src/ExceptionHandling/Demo.java
@@ -0,0 +1,27 @@
+package ExceptionHandling;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Enter a whole number to divide : ");
+            int x = scanner.nextInt();
+
+            System.out.println("Enter a whole number to divide by : ");
+            int y = scanner.nextInt();
+
+            int z = x / y;
+        } catch (ArithmeticException e) {
+            //execution will jump here if it's related to ArithmeticException
+            System.out.println("U can't divide by zero" + e);
+        } catch (Exception e) {
+            //this will catch all exceptions
+            System.out.println("error");
+        } finally {
+            System.out.println("this will always print");
+        }
+    }
+}
---
+++ src/abstraction/Demo.java
@@ -0,0 +1,8 @@
+package abstraction;
+
+public class Demo {
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        car.go();
+    }
+}
